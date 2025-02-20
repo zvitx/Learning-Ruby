6cf06054f38e941f162ad850fddaf13aecbbc91b
Vitor Abreu <vitu.abreu09@gmail.com>
2025-02-20 12:05:58 -0300

    comentários loop for

--- repeticao/for.rb
+++ repeticao/for.rb
@@ -6,6 +6,6 @@ for name in nomes do #estrutura padrão para for de um vetor
     puts name
 end
 
-for key, value in dict do
-    puts "#{key}: #{value}"
+for key, value in dict do #estrutura padrão para for de uma hash, percorre a chave e o valor
+    puts "#{key}: #{value}" #formatação de exibição
 end
